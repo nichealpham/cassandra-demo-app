b347c951dc9490c964ee2c89d5065732f1618ddc
Nguyen Pham <nichealpham@gmail.com>
2021-01-04 12:41:02 +0700

    Update readme

--- node_modules/express-status-monitor/src/helpers/send-metrics.js
+++
@@ -1,8 +0,0 @@
-module.exports = (io, span) => {
-  io.emit('esm_stats', {
-    os: span.os[span.os.length - 2],
-    responses: span.responses[span.responses.length - 2],
-    interval: span.interval,
-    retention: span.retention,
-  });
-};
